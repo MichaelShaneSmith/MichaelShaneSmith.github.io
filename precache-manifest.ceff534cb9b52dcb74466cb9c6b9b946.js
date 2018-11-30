45c08cb8bff2a33bab68078dd01e90087f39c74d
M. Smith <animaniac21@gmail.com>
2018-11-30 13:32:18 -0600

    Updates

---
+++ precache-manifest.ceff534cb9b52dcb74466cb9c6b9b946.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "59fc870b08d397fd0a85",
+    "url": "/react-gh-pages/static/css/main.d7338406.chunk.css"
+  },
+  {
+    "revision": "59fc870b08d397fd0a85",
+    "url": "/react-gh-pages/static/js/main.59fc870b.chunk.js"
+  },
+  {
+    "revision": "2dd891c45704ef41f3d5",
+    "url": "/react-gh-pages/static/js/1.2dd891c4.chunk.js"
+  },
+  {
+    "revision": "346eb3eec4c1b18bf79d",
+    "url": "/react-gh-pages/static/js/runtime~main.346eb3ee.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-gh-pages/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "c6134059d7391ca02b6516fbf51c8573",
+    "url": "/react-gh-pages/index.html"
+  }
+];
